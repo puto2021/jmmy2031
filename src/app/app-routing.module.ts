3562048507f23e9238dc0d91c0fb1e6c7c1c39a9
puto <kayyser2002@yahoo.com>
2020-11-12 02:04:42 -0300

    iniciar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  {path: '', pathMatch :'full', redirectTo: 'inicio'},
+  {path: 'inicio', component: InicioComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
